c8ff8994cb814ec54fdec074cbbe381ba5e6700e
Victor Giron <victor.giron@blackhill.dev>
2022-01-05 17:19:33 +0100

    test

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const myRecipe = [{
+  title:"Scrambled Eggs",
+  level: 'Easy Peasy',
+  ingredients : ["Eggs","More eggs"],
+  cuisine : "cuisine field",
+  dishType: "breakfast",
+  creator: "Victor"
+}]
+
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +26,18 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe
+      .insertMany(myRecipe)
+      .then(() => console.log("I am here"))
+      .then(()=>{
+        Recipe.insertMany(data)
+      }
+
+      )
+      
+    
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
